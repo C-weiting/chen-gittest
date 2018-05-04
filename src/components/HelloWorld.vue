80011c8c7cdc1d05c0ed996a84e1b136f2b745a0
cwtwt <1219576162@qq.com>
2018-05-04 10:18:20 +0800

    dd

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+      <el-button @click="add">增加</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  created () {
+     this.init();
+  },
+  data () {
+    return {
+      msg: {
+        name: 'chen'
+      }
+    }
+  },
+  methods: {
+    init (){
+      console.log(this.$store.state)
+    },
+    add () {
+      this.$store.commit('increment')
+      console.log(this.$store.state.count)
+      this.$set(this.msg, 'age', 18);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(Vuex)
+Vue.use(ElementUI);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+const vm = new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
